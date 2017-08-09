3b6ae5fa742d0c505922fb2069059339c160ec37
anne <richardson.ae@gmail.com>
2017-08-08 19:32:01 -0500

    Set up routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,18 @@
 var express = require('express');
-var router = express.Router();
+
+// Here we build the router.
+var router = express.Router()
+const {gods} = require('../models/gods')
+
+// Notice how we're calling get on the router
+// rather than on the application
+router.get('/', function(req, res, next) {
+  res.render('index', { gods: gods })
+})
+
+// Finally, we return the router so we can
+// use it in our app.js
+module.exports = router
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
